046d24712cf5de039bf0db29b534fcb17d3c7569
patcurry <currypat1985@gmail.com>
2018-02-01 10:09:40 +0100

    how do i premake an index.js file?

--- blog/static/index.js
+++ blog/static/index.js
@@ -1,4 +1,7 @@
 ReactDOM.render(
-    <h1>This is react.</h1>,
+    <div>
+        <h1>This is react.</h1>
+        <h2>So, how do I compile all the react stuff into a single index.js file?</h2>
+    </div>,
     document.getElementById('react')
 );
